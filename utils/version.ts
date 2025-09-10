df6231f99b346737f936c382720d992d2fd526f9
Toastmasters Scheduler <user@toastmasters-scheduler.app>
2025-09-10 00:50:51 -0700

    Bump version to 9.10.25-1

--- utils/version.ts
+++ utils/version.ts
@@ -37,7 +37,7 @@ const generateVersion = (): string => {
 };
 
 // Current version - this will be updated automatically
-const CURRENT_VERSION = "9.09.25-7";
+const CURRENT_VERSION = "9.10.25-1";
 
 export const getAppVersion = (): string => {
   return CURRENT_VERSION;
