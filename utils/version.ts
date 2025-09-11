2313ef79a2428f95412e8dbb717024fffb75b4bc
Professionally <tmprofessionallyspeaking@gmail.com>
2025-09-11 12:16:40 -0700

    chore: auto-update version before push

--- utils/version.ts
+++ utils/version.ts
@@ -37,7 +37,7 @@ const generateVersion = (): string => {
 };
 
 // Current version - this will be updated automatically
-const CURRENT_VERSION = "9.11.25-17";
+const CURRENT_VERSION = "9.11.25-18";
 
 export const getAppVersion = (): string => {
   return CURRENT_VERSION;
